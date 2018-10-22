39ac55522b3517b695e6b3b0f6be28c602a45c0a
lamx710 <lamx710@gmail.com>
2018-10-22 15:48:17 +0700

    delete old file

--- Model/Config/Source/CategoryList.php
+++ Model/Config/Source/CategoryList.php
@@ -10,22 +10,61 @@ class CategoryList implements \Magento\Framework\Option\ArrayInterface
 {
 
     protected $_categories;
+    protected $_categoryFactory;
+    protected $_category;
 
-    public function __construct(\Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $collection)
-    {
+    public function __construct(
+        \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $collection,
+        \Magento\Catalog\Model\CategoryFactory $categoryFactory
+    ) {
         $this->_categories = $collection;
+        $this->_categoryFactory = $categoryFactory;
     }
 
+    /**
+     * @return array
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
     public function toOptionArray()
     {
         $collection = $this->_categories->create();
-        $collection->addAttributeToSelect('*')->addFieldToFilter('is_active', 1);
+        $collection->addAttributeToSelect('*')
+            ->addFieldToFilter('is_active', 1);
+        //->addAttributeToFilter('is_anchor', 1)
         $itemArray = ['value' => '', 'label' => '-- Select --'];
-        $options = [];
-        $options = $itemArray;
+        $options[] = $itemArray;
+        $parentName = '';
         foreach ($collection as $category) {
-            $options[] = ['value'=>$category->getId(), 'label' => $category->getName()];
+            if ($category->getData('is_anchor') == 1) {
+                $parentNameArr = $this->getCategory($category->getId())->getParentCategories();
+                foreach ($parentNameArr as $parent) {
+                    if ($parent->getLevel() == 2) {
+                        $parentName = $parent->getName() . " > ";
+                    }
+                }
+
+                if (!empty($parentName)) {
+                    $options[] = ['value'=>$category->getId(), 'label' => $parentName.$category->getName()];
+                } else {
+                    $options[] = ['value'=>$category->getId(), 'label' => $category->getName()];
+                }
+            }
         }
         return $options;
     }
+
+    /**
+     * Get category object
+     * Using $_categoryFactory
+     *
+     * @param $categoryId
+     * @return \Magento\Catalog\Model\Category
+     */
+    public function getCategory($categoryId)
+    {
+        $this->_category = $this->_categoryFactory->create();
+        $this->_category->load($categoryId);
+        return $this->_category;
+    }
+
 }
